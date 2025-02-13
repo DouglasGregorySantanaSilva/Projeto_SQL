bc360667488a60f6a67c407c80c46143bcf2dcbc
Douglas Gregory de Santana Silva <douglasgregoryssi@gmail.com>
2025-02-13 15:06:02 -0300

    1.2

--- Query1.sql
+++ Query1.sql
@@ -1,3 +1,5 @@
+-- Esta consulta retorna os 10 maiores salários para as posições de analista de dados, que são de regime exclusivamente remoto
+
 SELECT jpf.job_id, cd.name as Company_Name, jpf.job_title, jpf.job_location,  jpf.job_via,  jpf.job_schedule_type,  jpf.search_location,  jpf.job_posted_date,  jpf.salary_year_avg
 FROM job_postings_fact as jpf
 LEFT JOIN company_dim as cd ON jpf.company_id = cd.company_id
