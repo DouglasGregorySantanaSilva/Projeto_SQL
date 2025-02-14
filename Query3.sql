ceb297d0963acc356a49a5319c6abdc395836c84
Douglas Gregory de Santana Silva <douglasgregoryssi@gmail.com>
2025-02-14 15:40:22 -0300

    1.0

---
+++ Query3.sql
@@ -0,0 +1,10 @@
+-- O objetivo desta consulta é analizar as habilidades mais requisitadas para um analista de dados pelo mercado de trabalho
+
+SELECT skills, COUNT(skills) as Incidence
+FROM skills_dim AS sd
+INNER JOIN skills_job_dim AS sjd ON sjd.skill_id = sd.skill_id
+INNER JOIN job_postings_fact AS jpf ON jpf.job_id = sjd.job_id
+WHERE jpf.job_title LIKE '%Data Analyst%' AND jpf.salary_year_avg IS NOT NULL
+GROUP BY sd.skills
+ORDER BY COUNT(skills) desc
+LIMIT 10;
