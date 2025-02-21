10f81c2f5ead12d1441f1a08a7fbd6d3280cf8c7
Douglas Gregory de Santana Silva <douglasgregoryssi@gmail.com>
2025-02-21 12:09:28 -0300

    1.0

--- Empregos_Mais_Bem_Remunerado.sql
+++
@@ -1,8 +0,0 @@
--- Esta consulta retorna os 10 maiores salários para as posições de analista de dados, que são de regime exclusivamente remoto
-
-SELECT jpf.job_id, cd.name as Company_Name, jpf.job_title, jpf.job_location,  jpf.job_via,  jpf.job_schedule_type,  jpf.search_location,  jpf.job_posted_date,  jpf.salary_year_avg
-FROM job_postings_fact as jpf
-LEFT JOIN company_dim as cd ON jpf.company_id = cd.company_id
-WHERE ((jpf.job_title like '%Data Analyst%' and jpf.job_location = 'Anywhere') and jpf.salary_year_avg is not null)
-ORDER BY jpf.salary_year_avg desc
-LIMIT 10;
